0fdb5296d38ff93edebf143e8558150aa2a0698e
Fauza Aulia <fauza@students.unnes.ac.id>
2019-11-07 13:48:39 +0700

    update db

--- db_updown.sql
+++ db_updown.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Okt 2019 pada 15.31
+-- Waktu pembuatan: 07 Nov 2019 pada 07.48
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.4
 
@@ -25,17 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `file`
+-- Struktur dari tabel `files`
 --
 
-CREATE TABLE `file` (
+CREATE TABLE `files` (
   `id` int(11) NOT NULL,
   `tanggal_upload` int(11) NOT NULL,
-  `nama_file` int(11) NOT NULL,
-  `tipe_file` int(11) NOT NULL,
-  `ukuran_file` int(11) NOT NULL,
-  `file` int(11) NOT NULL
+  `nama_file` varchar(128) NOT NULL,
+  `tipe_file` varchar(128) NOT NULL,
+  `ukuran_file` varchar(128) NOT NULL,
+  `file` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `files`
+--
+
+INSERT INTO `files` (`id`, `tanggal_upload`, `nama_file`, `tipe_file`, `ukuran_file`, `file`) VALUES
+(12, 1573001004, 'me', '.JPG', '772.77', 'IMG_9405.JPG'),
+(13, 1573091127, 'des', '.docx', '11.75', 'Tugas_DES.docx');
 
 -- --------------------------------------------------------
 
@@ -60,8 +68,48 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`, `role_id`, `images`, `is_active`, `date_created`) VALUES
-(5, 'Ahmad Fauza Aulia', 'fauzaaulia', 'fauzaauliia@gmail.com', '$2y$10$YAu/xa52dcenCosxsHdLGuwfTpIOBZFqPogcQ9ILuWlFZneQb/lqu', 2, 'default.JPG', 1, 1572025996),
-(7, 'Aa', 'aa', 'aa@aa.aa', '$2y$10$bTAGncRUzJLcxdAJczv79ONkBlj5tjjTeshgCabyorxIIc8ruT/16', 2, '', 1, 1572442677);
+(5, 'Ahmad Fauza Aulia', 'fauzaaulia', 'fauzaauliia@gmail.com', '$2y$10$YAu/xa52dcenCosxsHdLGuwfTpIOBZFqPogcQ9ILuWlFZneQb/lqu', 1, 'default.JPG', 1, 1572025996),
+(7, 'Aa', 'aa', 'aa@aa.aa', '$2y$10$bTAGncRUzJLcxdAJczv79ONkBlj5tjjTeshgCabyorxIIc8ruT/16', 2, 'default.JPG', 1, 1572442677);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User');
 
 -- --------------------------------------------------------
 
@@ -82,14 +130,39 @@ INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
 (2, 'Member');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fa fa-dashboard', 1),
+(2, 2, 'List Files', 'user/files', 'fa fa-th-list', 1),
+(3, 1, 'List Member', 'admin/member', 'fa fa-user', 1),
+(4, 2, 'Profil', 'user', 'fa fa-user', 1);
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `file`
+-- Indeks untuk tabel `files`
 --
-ALTER TABLE `file`
+ALTER TABLE `files`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -99,9 +172,27 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -109,10 +200,10 @@ ALTER TABLE `user_role`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `file`
+-- AUTO_INCREMENT untuk tabel `files`
 --
-ALTER TABLE `file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
@@ -121,10 +212,28 @@ ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT untuk tabel `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
